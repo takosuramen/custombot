e6035c6432da205f7093c7e5bfa2da46aa22c7c0
takosuramen <100332992+takosuramen@users.noreply.github.com>
2022-03-07 11:58:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ import traceback
 import random
 
 # discord.Intents.members = True  # これをしないとget_memberとかできなくなる
-bot = commands.Bot(command_prefix='$', intents=Intents.discord.all(), help_command=None)
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.all(), help_command=None)
 
 guild_ID = 892796029362139166
 guild = bot.get_guild(guild_ID)
