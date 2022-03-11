aaef972ad35aa6c71c8123f9e154e2fc8d21182a
takosuramen <100332992+takosuramen@users.noreply.github.com>
2022-03-11 22:07:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,8 +24,8 @@ async def on_command_error(ctx, error):  # エラーはいたときに教えて�
 
 @bot.event
 async def on_ready():  # BOT起動時にメッセージを送る
-    dt = datetime.datetime.now() + timedelta(hours=9)
-    jst = datetime.datetime.strftime(dt, '%Y-%m-%d %H:%M:%S')
+    dt = datetime.datetime.now()
+    jst = datetime.datetime.strftime(dt, '%Y-%m-%d %H:%M:%S') + timedelta(hours=9)
     await bot.change_presence(activity=discord.Game(name=f'起動日時{jst}'))
     # chan = bot.get_channel(BOT_COMMAND_CHANNEL_ID)
     # 再起動するたびにうるさいので一回消しとく await chan.send("準備完了! $help でコマンドを確認できるよ")
