48455f76d34fed80f62e17339fbd1c4d4765b896
takosuramen <100332992+takosuramen@users.noreply.github.com>
2022-04-11 21:35:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -100,10 +100,10 @@ async def custom(ctx, num1: int = 5, num2: int = 5):  # カスタムチーム分
         redmem = await guild.fetch_member(redteam[i])
         await redmem.move_to(red_team)
 
-    embed = discord.Embed(title='カスタムチーム分け', color=0xfc7b03)
-    embed.add_field(name='青チーム', value=[name + '/n' for name in bluemem.display_name], inline=True)
-    embed.add_field(name='赤チーム', value=[name + '\n' for name in redmem.display_name], inline=True)
-    await ctx.send(embed=embed)
+    # embed = discord.Embed(title='カスタムチーム分け', color=0xfc7b03)
+    # embed.add_field(name='青チーム', value=[name + '\n' for name in bluemem.display_name], inline=True)
+    # embed.add_field(name='赤チーム', value=[name + '\n' for name in redmem.display_name], inline=True)
+    # await ctx.send(embed=embed)
     # message = "-----赤チーム-----" + [bot.get_user(redteam[ID]).display_name for ID in len(redteam)] + "-----青チーム-----" + [bot.get_user(blueteam[ID]).display_name for ID in len(blueteam)]
     # await ctx.send(message)
     # await ctx.send(*[bot.get_user(ID).display_name for ID in user_ID])
