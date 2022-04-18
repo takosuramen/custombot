b9e88b4af8fbe0b1bda920faf06c0dba9c4a8628
takosuramen <100332992+takosuramen@users.noreply.github.com>
2022-04-18 14:33:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -117,7 +117,7 @@ async def custom(ctx, num1: int = 5, num2: int = 5):  # カスタムチーム分
 
 @bot.command()
 async def rank(ctx, *args):
-    # RiotのAPIサーバーがおちている時はbadrequestを返す。それを判別して返答する機能も欲しい
+    # RiotのAPIサーバーがおちている時はbadrequestを返す?それを判別して返答する機能も欲しい
     watcher = LolWatcher(key)
     """
     情報を取得するリージョン（地域）とユーザー名を設定
